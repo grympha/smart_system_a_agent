284df81f6d01c8008189458f51c08d0183067342
Grympha <grympha@gmail.com>
2026-06-01 18:56:27 +0800

    Add MT5 bridge startup script

---
+++ start_mt5_bridge.ps1
@@ -0,0 +1,19 @@
+param(
+    [Parameter(Mandatory = $true)]
+    [string]$ApiKey,
+
+    [string]$TerminalPath = "C:\Program Files\RoboForex MT5 Terminal\terminal64.exe",
+    [string]$HostAddress = "127.0.0.1",
+    [int]$Port = 5055
+)
+
+$ErrorActionPreference = "Stop"
+$ProjectRoot = Split-Path -Parent $MyInvocation.MyCommand.Path
+
+$env:MT5_BRIDGE_API_KEY = $ApiKey
+$env:MT5_TERMINAL_PATH = $TerminalPath
+$env:MT5_BRIDGE_HOST = $HostAddress
+$env:MT5_BRIDGE_PORT = [string]$Port
+
+Set-Location $ProjectRoot
+python mt5_bridge.py
